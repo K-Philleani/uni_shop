4f5b712699ea6462013bf06e6e0e6585de192c63
K-Philleani <964202623@qq.com>
2020-11-17 20:30:34 +0800

    推荐和歌手部分

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,20 +1,36 @@
 <template>
 	<view>
 		<view>
-			<view>
+			<view class="banner">
 				<swiper 
-					:indicator-dots="true" 
 					:autoplay="true" 
 					:interval="5000" 
 					:duration="1000"
 					class="swiper"
 				>
 					<swiper-item v-for="(item, index) in bannerList" :key="index" class="swiper-item">
-						<view>
-							<image :src="item.pic" class="swiper-image"></image>
-						</view>
+						<!-- <image :src="item.pic" mode="heightFix" class="swiper-image"></image> -->
+						<view class="swiper-image" :style="{backgroundImage: 'url(' + item.pic + ')'}"></view>
 					</swiper-item>
 				</swiper>
+			</view>
+			
+			<view class="music-list">
+				<view class="list-title">推荐歌单</view>
+				<view class="list-block">
+					<view 
+						class="list-item" 
+						v-for="(item, index) in songList" 
+						:key="index"
+					>
+						<view>
+							<image :src="item.picUrl" mode="" class="list-image"></image>
+						</view>
+						<view class="list-name">
+							{{item.name}}
+						</view>
+					</view>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -24,21 +40,30 @@
 	export default {
 		data() {
 			return {
-				bannerList: []
+				bannerList: [],
+				songList: []
 			}
 		},
 		onLoad() {
 			this.getBannerList()
+			this.getMusicList()
 		},
 		methods: {
 			getBannerList() {
 				this.$requestApi({
 					url: "/banner",
 					params: {
-						type: 1
+						type: 3
 					}
 				}).then(res => {
 					this.bannerList = res.banners
+				})
+			},
+			getMusicList() {
+				this.$requestApi({
+					url: '/personalized'
+				}).then(res => {
+					this.songList = res.result
 				})
 			}
 		}
@@ -46,17 +71,39 @@
 </script>
 
 <style>
-	.swiper {
-		width: 750rpx;
-		height: 460rpx;
-	}
-	.swiper-item {
-		border-radius: 0 0 10px 10px;
+	.banner {
+		width: 700rpx;
+		margin: 10rpx auto;
 	}
 	.swiper-image {
-		width: 100%;
+		width: 700rpx;
+		height: 400rpx;
+		background-size: contain;
+		background-repeat: no-repeat;
 	}
-
+	
+	
+	.list-title {
+		margin: 5px
+	}
+	.list-block {
+		display: flex;
+		justify-content: space-around;
+		flex-wrap: wrap;
+		margin-top: 10rpx;
+	}
+	.list-item {
+		margin-top: 4rpx;
+		width: 230rpx;
+		height: 380rpx;
+	}
+	.list-image {
+		width: 230rpx;
+		height: 268rpx;
+	}
+	.list-name {
+		font-size: 22rpx;
+	}
 
 	
 </style>
--- pages/singers/singers.vue
+++ pages/singers/singers.vue
@@ -1,22 +1,81 @@
 <template>
 	<view>
-		购物车
+		<view></view>
+		
+		<view>
+			<view class="nav-bar">
+				<text :class="{'nav-bar-checked': field == 1 }" class="nav-bar-item" @click="selectFields(1)">华语</text>
+				<text :class="{'nav-bar-checked': field == 2 }" class="nav-bar-item" @click="selectFields(2)">欧美</text>
+				<text :class="{'nav-bar-checked': field == 3 }" class="nav-bar-item" @click="selectFields(3)">韩国</text>
+				<text :class="{'nav-bar-checked': field == 4 }" class="nav-bar-item" @click="selectFields(4)">日本</text>
+			</view>
+			<scroll-view scroll-y="true" >
+				<view class="list-item" v-for="(item, index) in singerList" :key="index">
+					<image :src="item.picUrl" mode="" class="list-images" lazy-load="true"></image>
+					<view>{{item.name}}</view>
+				</view>
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
 <script>
 	export default {
+		onLoad() {
+			this.getSingers(this.field)
+		},
 		data() {
 			return {
-				
+				singerList: [],
+				field: 1
 			}
 		},
 		methods: {
-			
+			getSingers(index) {
+				this.$requestApi({
+					url: '/toplist/artist',
+					params: {
+						type: index
+					}
+				}).then(res => {
+					this.singerList = res.list.artists
+				})
+			},
+			selectFields(index) {
+				this.field = index
+				this.singerList = []
+				this.getSingers(this.field)
+			}
 		}
 	}
 </script>
 
 <style>
-
+	.nav-bar {
+		width: 750rpx;
+		height: 100rpx;
+		padding: 10rpx 0 0 20rpx;
+		line-height: 100rpx;
+	}
+	.nav-bar-item {
+		margin: 0 20rpx;
+		/* cursor: pointer; */
+	}
+	.nav-bar-checked{
+		color: #B50E03;
+	}
+.list-item {
+	width: 750rpx;
+	height: 120rpx;
+	border-bottom: 1px solid #DDDDDD;
+	padding: 20rpx;
+	display: flex;
+	align-items: center;
+}
+.list-images {
+	width: 120rpx;
+	height: 120rpx;
+	border-radius: 16rpx;
+	margin-right: 40rpx;
+}
 </style>
